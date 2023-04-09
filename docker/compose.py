363545f1d59584fd9201db51f8e901a03385d336
Nicola <nicola.bovolato@gmail.com>
2023-04-09 18:43:17 +0200

    Fix compose script

--- docker/compose.py
+++ docker/compose.py
@@ -7,11 +7,11 @@ app_name                  = 'todo_app'
 ltapi_container_name      = 'lt-api'
 ltfrontend_container_name = 'lt-frontend'
 frontend_container_name   = 'frontend'
-compose_args              = f'-p {app_name}'
+compose_args              = ['-p', app_name]
 
 def error(msg: str):
   print(msg)
-  subprocess.check_call(['docker-compose', compose_args, 'down'], stdout=subprocess.STDOUT, stderr=subprocess.STDOUT)
+  subprocess.check_call(['docker-compose', *compose_args, 'down'], stdout=subprocess.STDOUT, stderr=subprocess.STDOUT)
   quit()
 
 def find_url(str: str):
@@ -28,21 +28,21 @@ os.environ['USE_LOCALTUNNEL'] = 'true'
 
 # stop the services
 print('Stopping services...')
-subprocess.check_call(['docker-compose', compose_args, 'down'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+subprocess.check_call(['docker-compose', *compose_args, 'down'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
 # rebuild the frontend image
 print('Rebuilding frontend image...')
-subprocess.check_call(['docker-compose', compose_args, 'build', frontend_container_name], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+subprocess.check_call(['docker-compose', *compose_args, 'build', frontend_container_name], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
 
 # restart the services
 print('Restarting services...')
-subprocess.check_call(['docker-compose', compose_args, 'up', '-d'], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+subprocess.check_call(['docker-compose', *compose_args, 'up', '-d', "--wait"], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
 
 # check if the subdomain is in the localtunnel container logs
-ltapi_logs = subprocess.check_output(['docker-compose', compose_args, 'logs', ltapi_container_name])
+ltapi_logs = subprocess.check_output(['docker-compose', *compose_args, 'logs', ltapi_container_name])
 if(api_subdomain not in ltapi_logs.decode('UTF-8')):
   error('Wrong api domain found, run again the script')
 
 # get the web app url and print it
-ltfrontend_logs = subprocess.check_output(['docker-compose', compose_args, 'logs', ltfrontend_container_name])
+ltfrontend_logs = subprocess.check_output(['docker-compose', *compose_args, 'logs', ltfrontend_container_name])
 print('App url: ' + find_url(ltfrontend_logs.decode('UTF-8')))
